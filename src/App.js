eb2920cf772663b0310121ed02478faffefbcadf
007arunwilson <007arunwilson@gmail.com>
2018-06-10 15:23:01 +0530

    Given a Theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="logo-container">
+            <img src={logo} className="App-logo" alt="logo" />
+            <span className="span-plus" > + </span>
+            <img
+              src={
+                "https://mms.businesswire.com/media/20180509006648/en/656774/23/AWS_logo_RGB.jpg"
+              }
+              className="App-logo-2"
+              alt="logo"
+            />
+          </div>
+          <h1 className="App-title">Welcome to React with AWS Cognito</h1>
+          <p>Use aws sdk safely in frontend with cognito poolid</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
