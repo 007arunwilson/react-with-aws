d1c43843926844d42d6f4ec5bf9a844ca49c1ef3
007arunwilson <007arunwilson@gmail.com>
2018-06-12 00:32:49 +0530

    Added Config File

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import configObject from "./config";
+global.config = configObject;
 
 ReactDOM.render(
   <Router>
